963b7b8d9a9f81c03a536f834736b6d412f7951d
HamzaRaza5 <hamzakashmiri2001@gmail.com>
2023-09-01 18:46:29 +0500

    created index.html and app.js for Task

---
+++ app.js
@@ -0,0 +1,59 @@
+// <------------ ALERTS ------------>
+
+//  Task 01
+//  alert("Welcome to our Website!");
+
+// Task 02
+// alert("Error! Please enter a valid passsword");
+
+// Task 03
+// alert("Welcome to JS Land...\nHappy Coding!");
+
+// Task 04
+// alert("Welcome to JS Land...");
+// alert("Happy Coding! \n▢ Prevent this page from creating additional dialogs.");
+
+// Task 05
+// alert("Hello... I can run JS through my web browser's console.");
+
+// <------------ VARIABLES FOR STRINGS ------------>
+
+// Task 01
+// var username;
+
+// Task 02
+// var myName = "Hamza Raza";
+
+// Task 03
+// var message;
+// message = "Hello World";
+// alert(message);
+
+// Task 04
+// var studentName = "jhone Doe";
+// var studentAge = "15 years old";
+// var studentQualification = "Certified Mobile Application Development";
+
+// alert(studentName);
+// alert(studentAge);
+// alert(studentQualification);
+
+// Task 05
+// var alertMessage = "PIZZA \nPIZZ \nPIZ \nPI \nP";
+// alert(alertMessage);
+
+// Task 06
+// var email = "hamzakashmiri2001@gmail.com";
+// alert("My email address is " + email);
+
+// Task 07
+// var book = "A smarter way to learn JavaScript";
+// alert("I am trying to learn from the Book " + book);
+
+// Task 08
+// document.write("Yah! I can write HTML content through JavaScript");
+
+// Task 09
+var specialChar = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+alert(specialChar);
+document.write("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
